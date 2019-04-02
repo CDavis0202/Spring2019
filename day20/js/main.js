f78f96deb2b27abf1aaa73df3222e23ce1b5fbc0
dickbutt <cdavis42@masonlive.gmu.edu>
2019-04-02 13:08:19 -0400

    w

--- day20/js/main.js
+++ day20/js/main.js
@@ -24,15 +24,25 @@ $(document).ready(function() {
     $('#game-3').toggleClass('lightblue');
   });
   $('#number3').mouseenter(function() {
-    $('#game-3').css('font-size', '2em');
+    $('#number3').css('border', '10px solid black');
   });
   $('#number3').mouseleave(function() {
-    $('#game-3').css('font-size', '1em');
+    $('#number3').css('border', '0px solid black');
+  });
+  $('#game-3').click(function() {
+    $('#number3').css('border', '10px solid orange');
   });
 
   $('#game-2').click(function() {
     $('#game-2').toggleClass('brown');
   });
+  $('#number2').mouseenter(function() {
+    $('#number2').css('text-decoration', 'overline');
+  });
+  $('#number2').mouseleave(function() {
+    $('#number2').css('text-decoration', 'none');
+  });
+
 
   $('#game-1').click(function() {
     $('#game-1').toggleClass('darkblue');
